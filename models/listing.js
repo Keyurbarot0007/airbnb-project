b6f44b785bd5c89dd1b0c2085b102dfc74df043b
Keyurbarot0007 <keyurbarot0007@gmail.com>
2024-02-13 19:00:50 +0530

    Initial Commit

---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: function(v) {
+      if (typeof v === 'object' && v.url) {
+        return v.url; // Assuming 'url' is the property containing the image URL
+      } else if (v === "") {
+        return this.default;
+      } else {
+        return v;
+      }
+    }
+  },  
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
